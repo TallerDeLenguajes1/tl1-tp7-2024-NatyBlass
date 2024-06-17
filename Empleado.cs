caea8e88eddac0d0f60b9119466ba9d18368d2e1
NatyBlass <natypaz12345@gmail.com>
2024-06-17 11:45:41 -0300

    Terminando Punto 2

--- Empleado.cs
+++ Empleado.cs
@@ -1,13 +1,93 @@
-namespace Empleados
+namespace DatosEmpleados
 {
 
+    public enum Cargos
+        {
+            Auxiliar,
+            Administrativo,
+            Ingeniero,
+            Especialista,
+            Investigador
+        }
+    public class Empleados
+    {
+        private string nombre;
+        private string apellido;
+        private DateTime fecNac;
+        private char estadoCiv;
+        private DateTime ingresoEmpresa;
+        private double sueldoBasico;
+        private Cargos cargo;
     
 
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public DateTime FecNac { get => fecNac; set => fecNac = value; }
+        public char EstadoCiv { get => estadoCiv; set => estadoCiv = value; }
+        public DateTime IngresoEmpresa { get => ingresoEmpresa; set => ingresoEmpresa = value; }
+        public double SueldoBasico { get => sueldoBasico; set => sueldoBasico = value; }
+        public Cargos Cargo { get => cargo; set => cargo = value; }
+
+        public int CalculadoraDeAntiguedad(DateTime fechaIng)
+        {
+            DateTime fechaActual = DateTime.Today; // DateTime.Today me brinda la fecha actual
+            int antiguedad = fechaActual.Year - fechaIng.Year; 
+            int mes = fechaActual.Month - fechaIng.Month;
+
+            Console.WriteLine("Ingreso hace"+antiguedad+"años y"+mes+"meses");
+
+            return antiguedad;
+            
+        }
+
+        public int CalculadoraDeEdad(DateTime fecNac)
+        {
+            DateTime fechaActual = DateTime.Today;
+            
+            int edad = fechaActual.Year-fecNac.Year;
+
+            return edad;
+        }
+
+        public int CalculadoraDeJubilacion(DateTime fecNac)
+        {
+            int edad = CalculadoraDeEdad(fecNac);
+            int añosParaJubilarse = 65 - edad;
+
+            return añosParaJubilarse;
+        }
+
+        public float CalculadoraDeSalario(int sueldoBasico, Cargos cargo, char estadoCiv, int antiguedad)
+        {
+            float salario, adicional;
+
+            if (antiguedad <= 20 && antiguedad >= 0)
+            {
+                adicional = (sueldoBasico * antiguedad) / 100;
+            }
+            else
+            {
+                adicional = (sueldoBasico * 25) / 100;
+            }
+
+            if (cargo == Cargos.Especialista || cargo == Cargos.Ingeniero)
+            {
+                adicional = adicional + (adicional * 50) / 100;
+            }
+
+            if(estadoCiv == 'C' || estadoCiv == 'c')
+            {
+                adicional = adicional + 150000;
+            }
+
+            salario = sueldoBasico + adicional;
+
+            return salario;
+        }
 
 
 
-
-
-
+    }
+    
 
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
-using DatosEmpleados;
+using DatosEmpleados; 
 
 Empleados Empleado = new Empleados();
 int c = 0;
@@ -110,6 +110,56 @@ Console.WriteLine("Sueldo Basico:$"+Empleado.SueldoBasico);
 Console.WriteLine("Cargo: " +Empleado.Cargo);
 
 
+//EMPLEADOS EN ARREGLO
+
+DatosEmpleados.Empleados [] infEmpleados = new DatosEmpleados.Empleados[3];
+
+infEmpleados[0] = new DatosEmpleados.Empleados();
+infEmpleados[0].Nombre = "Juan";
+infEmpleados[0].Apellido = "Martinez";
+infEmpleados[0].FecNac = new DateTime(1980, 10, 20);
+infEmpleados[0].EstadoCiv = 'C';
+infEmpleados[0].IngresoEmpresa = new DateTime(2010, 5, 12);
+infEmpleados[0].SueldoBasico = 2500000;
+infEmpleados[0].Cargo = DatosEmpleados.Cargos.Ingeniero;
+
+infEmpleados[1] = new DatosEmpleados.Empleados();
+infEmpleados[1].Nombre = "Maria";
+infEmpleados[1].Apellido = "Lopez";
+infEmpleados[1].FecNac = new DateTime(1990, 7, 15);
+infEmpleados[1].EstadoCiv = 'S';
+infEmpleados[1].IngresoEmpresa = new DateTime(2015, 3, 8);
+infEmpleados[1].SueldoBasico = 1800000;
+infEmpleados[1].Cargo = DatosEmpleados.Cargos.Auxiliar;
+
+infEmpleados[2] = new DatosEmpleados.Empleados();
+infEmpleados[2].Nombre = "Pedro";
+infEmpleados[2].Apellido = "Garcia";
+infEmpleados[2].FecNac = new DateTime(1975, 12, 2);
+infEmpleados[2].EstadoCiv = 'C';
+infEmpleados[2].IngresoEmpresa = new DateTime(2005, 11, 1);
+infEmpleados[2].SueldoBasico = 3200000;
+infEmpleados[2].Cargo = DatosEmpleados.Cargos.Especialista;
 
 
+double totalSalarios = 0;
+
+foreach (DatosEmpleados.Empleados empleado in infEmpleados)
+{
+    Console.WriteLine("Nombre y Apellido: " + empleado.Nombre + " " + empleado.Apellido);
+    Console.WriteLine("Edad: " + empleado.CalculadoraDeEdad(empleado.FecNac) + " años");
+    Console.WriteLine("Antiguedad: " + empleado.CalculadoraDeAntiguedad(empleado.IngresoEmpresa) + " años");
+    Console.WriteLine("Cargo: " + empleado.Cargo);
+    Console.WriteLine("Sueldo: $" + empleado.CalculadoraDeSalario(empleado.SueldoBasico, empleado.Cargo, empleado.EstadoCiv, empleado.CalculadoraDeAntiguedad(empleado.IngresoEmpresa)));
+    Console.WriteLine("----------------------");
+
+    empleado.CalculadoraDeAntiguedad(empleado.IngresoEmpresa);
+    empleado.CalculadoraDeJubilacion(empleado.FecNac);
+
+    totalSalarios += empleado.CalculadoraDeSalario(empleado.SueldoBasico, empleado.Cargo, empleado.EstadoCiv, empleado.CalculadoraDeAntiguedad(empleado.IngresoEmpresa));
+}
+Console.WriteLine("Monto total a pagar de salarios: $" + totalSalarios);
+
+int edadJubilatoria = 65;
+DatosEmpleados.Empleados empleadoMasProximo = null;
 
